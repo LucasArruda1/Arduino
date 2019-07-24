ddce5e6eb94bb04ca86e0cae4a075ede2ffee18f
Lucas Arruda <lucas.arruda.cordeiro@gmail.com>
2019-07-24 15:35:40 -0300

    Serial interface changes

--- Finalized/2.Serial_Interface/Sinterface.h
+++ Finalized/2.Serial_Interface/Sinterface.h
@@ -1,7 +1,49 @@
 /*
-  .h - Library for flashing Morse code.
-  Created by David A. Mellis, November 2, 2007.
-  Released into the public domain.
+  Sinterface.h
+  
+  Creates an iterface with arduino and PC via serial port via command line. 
+
+  Example on command line:
+
+  ledOn
+  -- Led On! 
+
+
+  Use in code:
+
+  #include "Sinterface.h"
+
+  Sinterface InterfacePC;
+
+  void setup(){
+   Serial.begin(9600);
+  }
+
+  void serialEvent(){
+  //Code to run when there is data available on buffer
+    InterfacePC.getCmd();
+  }
+
+  void loop(){
+
+    //Comando simples
+    if(InterfacePC.analyze("Comando","Resposta terminal")){
+      //Code here 
+    }
+
+    //Comando do tipo tempo=1000 (Utilizo o valor depois do igual)
+    if(InterfacePC.analyze("tempo","Novo tempo configurado")){
+      time = atol(InterfacePC.receivedValue);   
+    }
+
+    //Configuro uma resposta caso não encontre um comando válido
+    InterfacePC.verifyNoCommands("Comando não reconhecido!",0)
+
+  }
+
+  Author: Lucas Cordeiro de Arruda. 
+  License: Personal.
+
 */
 
 #include "Arduino.h"
